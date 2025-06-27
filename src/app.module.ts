b67f156eba2fc2bccb73c3490b1ac5605cda589c
NataliaGadelha <natt.gdl25@gmail.com>
2025-06-27 10:23:35 -0300

    fix: corrigindo importação na classe app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -9,7 +10,7 @@ import { AppService } from './app.service';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'root',
+      password: '1234',
       database: 'db_crm',
       entities: [],
       synchronize: true,
