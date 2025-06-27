ed35d80ad05495c677ce4c0c1283250352430a94
taismichely <michely2207@gmail.com>
2025-06-27 12:27:12 -0300

    feat: Adicionado classe Categoria na classe AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Categoria } from './categoria/entities/categoria.entity';
+import { CategoriaModule } from './categoria/categoria.module';
 
 @Module({
   imports: [
@@ -11,9 +13,10 @@ import { AppService } from './app.service';
       username: 'root',
       password: 'root',
       database: 'db_crm',
-      entities: [],
+      entities: [Categoria],
       synchronize: true,
     }),
+    CategoriaModule
     // NomeModule,
   ],
   controllers: [AppController],
