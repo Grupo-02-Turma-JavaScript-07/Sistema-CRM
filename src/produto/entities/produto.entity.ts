4032d74bc3c02e4f170bf7ae3a71aa0bb94ea4ba
Natalia Taira <nataliagtaira@gmail.com>
2025-06-27 11:51:19 -0300

    Cria entidade produto com:
    - gerados de id;
    - nome e descrição com verificação de campo vazio;
    -  preco e quantidade com verificação de campo numérico;
    -  preco e quantidade com verificação de valor mínimo sendo 0;
    - padronização do uso de ponto e vírgula;

--- src/produto/entities/produto.entity.ts
+++ src/produto/entities/produto.entity.ts
@@ -1,1 +1,27 @@
-//
+import { IsNotEmpty, IsNumber, Min } from "class-validator";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("tb_produtos")
+export class Produto {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @IsNotEmpty()
+    @Column({length: 100, nullable: false})
+    nome: string;
+
+    @IsNotEmpty()
+    @Column({type: 'text', nullable: false})
+    descricao: string;
+
+    @IsNumber()
+    @Min(0)
+    @Column({type: 'decimal', precision: 10, scale: 2, nullable: false})
+    preco: number;
+
+    @IsNumber()
+    @Min(0)
+    @Column({type: 'bigint', nullable: false})
+    quantidade: number;
+}
