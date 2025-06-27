07ad20591a4558ddeeb014a2b1cfaf59ede1ae03
Naah Carvalho <naah_.carvalho@outlook.com>
2025-06-27 14:08:09 -0300

    feat: criei categoria Service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Categoria } from './categoria/entities/categoria.entity';
 import { CategoriaModule } from './categoria/categoria.module';
 
